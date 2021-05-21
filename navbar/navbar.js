176c027acf8ff0d9cb064b3e48f86fa1d32648a0
rina <74513590+kopefully@users.noreply.github.com>
2021-05-21 12:06:49 -0700

    Create navbar.js

---
+++ navbar/navbar.js
@@ -0,0 +1,10 @@
+	if('.g<!-- |g_id| -->' == '.g4') {
+		$("#staff-nav").removeClass("hidenav");
+		$("#staff-nav").addClass("shownav");
+	} else if('.g<!-- |g_id| -->' !== '.g4') {
+		$("#member-nav").removeClass("hidenav");
+		$("#member-nav").addClass("shownav");
+	} else if ('.g<!-- |g_id| -->' == '.g2') {
+		$("#guest-nav").removeClass("hidenav");
+		$("#guest-nav").addClass("shownav");
+  }
