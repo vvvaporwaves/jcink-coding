c129bc254cd24ba2c840dcc7981629ba34925af6
vee <74513590+vvvaporwaves@users.noreply.github.com>
2022-08-29 21:26:27 -0700

    Update INVU-tracker.js

--- cafeaulait/INVU-tracker.js
+++ cafeaulait/INVU-tracker.js
@@ -70,29 +70,3 @@ const makeRequest = (method, url) => {
     xhr.send();
   });
 };
-
-// tabs
-
-var tabLinks = document.querySelectorAll(".tablinks");
-var tabContent = document.querySelectorAll(".tabscontent");
-
-tabLinks.forEach(function (el) {
-  el.addEventListener("click", openTabs);
-});
-
-function openTabs(el) {
-  var btnTarget = el.currentTarget;
-  var country = btnTarget.dataset.country;
-
-  tabContent.forEach(function (el) {
-    el.classList.remove("active");
-  });
-
-  tabLinks.forEach(function (el) {
-    el.classList.remove("active");
-  });
-
-  document.querySelector("#" + country).classList.add("active");
-
-  btnTarget.classList.add("active");
-}
